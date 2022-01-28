7e9391f9d83c042fb1c9cf3bc3d1278e5fe70158
Edgar Nasarenko <Edgar.Nasarenko@sunzinet.com>
2022-01-28 17:33:45 +0100

    - SignUp angefangen

---
+++ resources/js/components/signup/SignUp.vue
@@ -0,0 +1,83 @@
+<template>
+
+    <v-card class="sign-up-card">
+
+        <validation-observer v-slot="{ handleSubmit }">
+            <v-form class="form" @submit.prevent="handleSubmit(signUp)">
+
+                <h1>Sign up</h1>
+
+                <validation-provider rules="required">
+                    <v-text-field
+                        label="Username"
+                        type="text"
+                        v-model="username"
+                    ></v-text-field>
+                </validation-provider>
+
+                <validation-provider rules="required">
+                    <v-text-field
+                        label="Password"
+                        type="password"
+                        v-model="password"
+                    ></v-text-field>
+                </validation-provider>
+
+                <v-btn>Sign up</v-btn>
+
+            </v-form>
+
+        </validation-observer>
+
+    </v-card>
+
+</template>
+
+<script>
+import { ValidationObserver, ValidationProvider } from 'vee-validate';
+import '../../ValidationRules/validationRules';
+
+export default {
+
+    name: "SignUp",
+    components: { ValidationObserver, ValidationProvider },
+    data() {
+        return {
+
+            username: null,
+            password: null
+
+        }
+    },
+    methods: {
+
+        signUp() {
+
+            let userData = {
+
+                username: this.username,
+                password: this.password
+
+            }
+
+            axios.post('/signup', userData);
+
+        }
+
+    }
+
+}
+</script>
+
+<style scoped>
+
+    .sign-up-card{
+
+        width: 50%;
+        margin: auto;
+        margin-top: 10%;
+        padding: 20px;
+
+    }
+
+</style>
